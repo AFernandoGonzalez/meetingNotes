d7cc57fbc836aa7f29ce0ced9d3b149e819e5ce1
Fernando <ferg.developer@gmail.com>
2023-12-13 18:56:55 -0500

    mongodb added to project

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,18 @@ import bodyParser from 'body-parser';
 import { standUpRouter } from './src/routes/standup.js';
 import { teamRouter } from './src/routes/team.js';
 
-// dotenv.config();
+import { connectDB } from './src/db/db.js';
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use('/api', standUpRouter);
 app.use('/api', teamRouter);
---
+++ backend/src/db/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.${process.env.DB_CLUSTER_ID}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGO_URI);
+        console.log(`MongoDB connected:!`);
+    } catch (error) {
+        console.log(error);
+    }
+};
